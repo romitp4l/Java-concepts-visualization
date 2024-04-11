a3d5f73d6adb4c6fbdef32f4df9714b6f86ea0e1
romitp4l <romitpal4@gmail.com>
2024-04-11 21:50:07 +0530

    explanation

--- src/OutOfBoundException.java
+++ src/OutOfBoundException.java
@@ -3,6 +3,11 @@ public class OutOfBoundException {
         int[] num = { 3,4,5,32};
 
         System.out.println(num[3]);
+
+
+        int intArray [];
+        intArray = new  int[4,5,5,4];
+
     }
 }
 
